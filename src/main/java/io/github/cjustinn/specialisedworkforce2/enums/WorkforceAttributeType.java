5ed815a3f19a80aeadb1129bc82dfb60f143d46b
cjustinn <such.justinn@gmail.com>
2023-10-03 21:17:54 -0400

    #8: Implement the bonus fishing drops attribute.

--- src/main/java/io/github/cjustinn/specialisedworkforce2/enums/WorkforceAttributeType.java
+++ src/main/java/io/github/cjustinn/specialisedworkforce2/enums/WorkforceAttributeType.java
@@ -3,5 +3,6 @@ package io.github.cjustinn.specialisedworkforce2.enums;
 public enum WorkforceAttributeType {
     BONUS_BLOCK_DROPS,
     BONUS_MOB_DROPS,
+    BONUS_FISHING_DROPS,
     DURABILITY_SAVE
 }
